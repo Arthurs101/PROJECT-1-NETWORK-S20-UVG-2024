961451bda6709009a5ddcea3ec4d8107bc5dfdc9
Arthurs101 <84546401+Arthurs101@users.noreply.github.com>
2024-08-06 20:47:16 -0600

    added XMPPConnection

--- xmpp_chat/src/main/java/com/xmpp_chat/xmpp_chat/services/XmppService.java
+++ xmpp_chat/src/main/java/com/xmpp_chat/xmpp_chat/services/XmppService.java
@@ -1,31 +1,34 @@
 package com.xmpp_chat.xmpp_chat.services;
 
+import java.io.IOException;
+
 import org.jivesoftware.smack.ConnectionConfiguration;
 import org.jivesoftware.smack.SmackException;
-import org.jivesoftware.smack.XMPPConnection;
+import org.jivesoftware.smack.XMPPException;
 import org.jivesoftware.smack.tcp.XMPPTCPConnection;
 import org.jivesoftware.smack.tcp.XMPPTCPConnectionConfiguration;
-import org.jivesoftware.smack.XMPPException;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-import java.io.IOException;
 
 @Service
 public class XmppService {
+    @Value("${xmpp.domain}")
+    private String xmppDomain;
 
-    private XMPPConnection connection;
+    private XMPPTCPConnection connection;
 
     public void connect(String username, String password) throws IOException, InterruptedException, SmackException, XMPPException {
         XMPPTCPConnectionConfiguration config = XMPPTCPConnectionConfiguration.builder()
                 .setUsernameAndPassword(username, password)
-                .setXmppDomain("yourdomain.com")
-                .setHost("hostname")
+                .setXmppDomain(xmppDomain)
+                // .setHost("hostname")
                 .setPort(5222)
                 .setSecurityMode(ConnectionConfiguration.SecurityMode.ifpossible)
                 .build();
 
         connection = new XMPPTCPConnection(config);
-        connection.connect();
+        connection.connect();  // This method is specific to XMPPTCPConnection
         connection.login();
     }
 
