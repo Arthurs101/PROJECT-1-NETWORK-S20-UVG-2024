eee29c69f66ae372162b91dc5616bb1e541d9d3a
Arthurs101 <84546401+Arthurs101@users.noreply.github.com>
2024-08-13 20:34:51 -0600

    added message sending implementation ( tested and works )

--- xmpp_chat/src/main/java/com/xmpp_chat/xmpp_chat/controller/XmppDashboardController.java
+++ xmpp_chat/src/main/java/com/xmpp_chat/xmpp_chat/controller/XmppDashboardController.java
@@ -1,12 +1,40 @@
 package com.xmpp_chat.xmpp_chat.controller;
 
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.xmpp_chat.xmpp_chat.services.XmppClient;
+
 
 @Controller
 public class XmppDashboardController {
+    @Autowired
+    private XmppClient xmppClient;
+
+
     @GetMapping("/dashboard")
     public String getDashboard() {
         return "dashboard";
     }
+    
+
+
+    @PostMapping("/message")
+    @ResponseBody
+    public String postMessage(@RequestBody Map<String, String> payload) {
+    String message = payload.get("message");
+    String destination = payload.get("destination");
+    System.out.println("Received message: " + message);
+    // Process the message here (e.g., broadcast to other users, save to DB, etc.)
+    try{xmppClient.sendMessage(destination,message);}
+    catch(Exception e){return e.getMessage();}
+    return "Message received: " + message;
+}
+    
 }
--- xmpp_chat/src/main/java/com/xmpp_chat/xmpp_chat/services/XmppClient.java
+++ xmpp_chat/src/main/java/com/xmpp_chat/xmpp_chat/services/XmppClient.java
@@ -6,19 +6,28 @@ import java.util.Map;
 import org.jivesoftware.smack.ConnectionConfiguration;
 import org.jivesoftware.smack.SmackException;
 import org.jivesoftware.smack.XMPPException;
+import org.jivesoftware.smack.chat2.Chat;
+import org.jivesoftware.smack.chat2.ChatManager;
+import org.jivesoftware.smack.roster.Roster;
 import org.jivesoftware.smack.tcp.XMPPTCPConnection;
 import org.jivesoftware.smack.tcp.XMPPTCPConnectionConfiguration;
 import org.jivesoftware.smackx.iqregister.AccountManager;
+import org.jxmpp.jid.EntityBareJid;
+import org.jxmpp.jid.impl.JidCreate;
 import org.jxmpp.jid.parts.Localpart;
+import org.jxmpp.stringprep.XmppStringprepException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-
-
 @Service
 public class XmppClient {
+    private XmppClient singleton = null;
+    public XmppClient getSingleton() { 
+        if (singleton == null) {
+            singleton = new XmppClient();
+    }return singleton; }
     private XMPPTCPConnection connection;
-
+    private AccountManager accountManager;
     @Value("${xmpp.domain}")
     private String domain;
 
@@ -57,7 +66,7 @@ public class XmppClient {
 
         connection = new XMPPTCPConnection(config);
         connection.connect();
-        AccountManager accountManager = AccountManager.getInstance(connection);
+        accountManager = AccountManager.getInstance(connection);
         accountManager.sensitiveOperationOverInsecureConnection(true);
         if (accountManager.supportsAccountCreation()) {
             Localpart localUsr = Localpart.from(username);
@@ -70,7 +79,48 @@ public class XmppClient {
             System.out.println("El servidor no soporta la creación de cuentas");
         }
         return false;
+}
+
+    public void sendMessage(String to, String messageBody) throws XmppStringprepException, SmackException.NotConnectedException, InterruptedException {
+        if (connection != null && connection.isAuthenticated()) {
+            ChatManager chatManager = ChatManager.getInstanceFor(this.connection);
+            System.out.println(to + "@" + this.domain);
+            Chat chat = chatManager.chatWith(JidCreate.entityBareFrom(to + "@" + this.domain));
+            chat.send(messageBody);
+            
+        } else {
+            System.out.println("No auth session available");
+        }
     }
-    // Add other methods to send/receive messages
+
+    public boolean DeleteAccount() {
+        if(this.accountManager == null){
+            return false;
+        }else{
+            try {
+                this.accountManager.deleteAccount();
+            }catch (Exception e){
+                return false;
+            }
+        }
+        return false;
+
+    }
+     
+    public  void agregarContacto(String jidStr) {
+        if (this.connection != null && this.connection.isAuthenticated()) {
+            try {
+                EntityBareJid jid = JidCreate.entityBareFrom(jidStr);
+                Roster roster = Roster.getInstanceFor(connection);
+                roster.createEntry(jid, jidStr, null);
+                System.out.println("Contacto agregado exitosamente.");
+            } catch (Exception e) {
+                System.out.println("Error al agregar contacto: " + e.getMessage());
+                e.printStackTrace();
+            }
+        } else {
+            System.out.println("No has iniciado sesión.");
+        }
+    }
 }
 
